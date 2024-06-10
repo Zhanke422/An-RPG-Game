4ec750579384a1e574079e53c5b6fb81cb71f74d
Zhanke422 <dzk0422@gmail.com>
2024-06-11 00:18:50 +0800

    Committed Graphics Test simple movements

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Move info")]
+    public float moveSpeed = 8f;
+
+    #region Components
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+
+    #endregion
+    #region States
     public PlayerStateMachine stateMachine { get; private set; }
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    #endregion
 
     public void Awake()
     {
@@ -19,11 +29,19 @@ public class Player : MonoBehaviour
 
     public void Start()
     {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
         stateMachine.Initialize(idleState);
     }
 
     private void Update()
     {
         stateMachine.currentState.Update();
+    }
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
     }
 }
--- Assets/PlayerIdleState.cs
+++ Assets/PlayerIdleState.cs
@@ -22,5 +22,8 @@ public class PlayerIdleState : PlayerState
     public override void Update()
     {
         base.Update();
+
+        if(xInput != 0)
+            stateMachine.ChangeState(player.moveState);
     }
 }
